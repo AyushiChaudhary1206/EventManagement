8042f34345ed8e64616aedf71c69fed20ab060ef
AyushiChaudhary1206 <154445679+AyushiChaudhary1206@users.noreply.github.com>
2025-02-12 12:19:08 +0530

    Update Login.jsx

--- eventfrontend/src/Components/Login.jsx
+++ eventfrontend/src/Components/Login.jsx
@@ -10,7 +10,7 @@ const Login = () => {
  const handleSubmit=async(e)=>{
   e.preventDefault();
   try {
-     const res=await axios.post("http://localhost:3001/api/v1/login", {
+     const res=await axios.post("https://eventmanagementbackend-hlij.onrender.com/api/v1/login", {
       email,
       password,
     },{
